d3ab4e175d5f88dd81dbea22649a2e33f4b0670a
sachshan <sachdeva.sh@northeastern.edu>
2024-06-26 22:55:32 -0400

    Add places feature

--- be/flaskr/map/utils.py
+++ be/flaskr/map/utils.py
@@ -1,6 +1,7 @@
 import googlemaps
 import json, os, time
 import dotenv
+import math
 
 dotenv.load_dotenv()
 
@@ -15,9 +16,24 @@ def get_places(location, radius, place_type):
         name = place.get('name')
         lat = place['geometry']['location']['lat']
         lng = place['geometry']['location']['lng']
-        locations.append({"user_name": name, "location":[lat, lng]})
+        distance = haversine(location[0], location[1], lat, lng)
+        locations.append({"user_name": name, "location":[lat, lng], "distance": distance})
+        locations = sorted(locations, key=lambda x: x['distance'])
 
     return locations
+
+
+def haversine(lat1, lon1, lat2, lon2):
+    # Convert latitude and longitude from degrees to radians
+    lat1, lon1, lat2, lon2 = map(math.radians, [lat1, lon1, lat2, lon2])
+
+    # Haversine formula
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+    a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    r = 6371  # Radius of Earth in kilometers. Use 3956 for miles. Determines return value units.
+    return r * c
 
     
 # print(get_places([42.34580914708963, -71.07590585947037], 15000, 'restaurant'))
--- fe/src/MapPage.js
+++ fe/src/MapPage.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { Map, Marker, Overlay } from 'pigeon-maps';
-import './css/MapPage.css'; // Import your Sass file
+import './css/MapPage.css';
 
 const MapPage = () => {
   const { username, group_key } = useParams();
   const [markerPosition, setMarkerPosition] = useState(null);
   const [usersLocations, setUsersLocations] = useState([]);
+  const [places, setPlaces] = useState([]);
+  const [place_type, setPlaceType] = useState('');
 
   useEffect(() => {
     const fetchUserLocation = () => {
@@ -110,27 +112,79 @@ const MapPage = () => {
     setMarkerPosition(location);
   };
 
+  const fetchPlaces = async (placeType) => {
+    if (placeType === 'restaurant') {
+      setPlaceType('Restaurants');
+    } else if (placeType === 'night_club') {
+      setPlaceType('Night Clubs');
+    } else if (placeType === 'cafe') {
+      setPlaceType('Cafes');
+    }
+
+    const serverUrl = process.env.REACT_APP_SERVER_URL;
+    if (!serverUrl) {
+      console.error('Server URL is not defined.');
+      return;
+    }
+
+    try {
+      const response = await fetch(`${serverUrl}/map/${placeType}/${group_key}`, {
+        headers: {
+          'ngrok-skip-browser-warning': 'true',
+        },
+      });
+      const data = await response.json();
+      setPlaces(data);
+    } catch (error) {
+      console.error('Error fetching places:', error);
+    }
+  };
+
   return (
-    <div className="map-container">
+    <div className='layout-container'>
       <h2>Pigeon Hangout App</h2>
-      <p>Welcome, {username} from group {group_key}!</p>
-      <Link to="/">Go Back to Login</Link>
-      <div className="map">
-        <Map center={markerPosition} zoom={12} width={1100} height={800}>
-          {usersLocations.map((user, index) => (
-            <Overlay key={index} anchor={user.location} offset={[0, 0]}>
-              <div
-                className="overlay"
-                onClick={() => handleNameClick(user.location)}
-              >
-                {user.user_name}
-              </div>
-            </Overlay>
+          <p>Welcome, {username} from group {group_key}!</p>
+          <Link to="/">Go Back to Login</Link>
+      <div className='top-container'>
+        <div className="map-container">
+          <div className="map">
+            <Map center={markerPosition} zoom={12} width={1100} height={800}>
+              {usersLocations.map((user, index) => (
+                <Overlay key={index} anchor={user.location} offset={[0, 0]}>
+                  <div
+                    className="overlay"
+                    onClick={() => handleNameClick(user.location)}
+                  >
+                    {user.user_name}
+                  </div>
+                </Overlay>
+              ))}
+              {places.map((place, index) => (
+                <Overlay key={index} anchor={place.location} offset={[0, 0]}>
+                  <div className="place-number" style={{ fontWeight: 'bold' }}>{index + 1}</div>
+                </Overlay>
+              ))}
+              {markerPosition && (
+                <Marker anchor={markerPosition} payload={username} onClick={({ event, anchor, payload }) => { 
+                  
+                }} />
+              )}
+            </Map>
+          </div>
+          <div className="place-buttons">
+            <button onClick={() => fetchPlaces('restaurant')}>Restaurants</button>
+            <button onClick={() => fetchPlaces('night_club')}>Night Clubs</button>
+            <button onClick={() => fetchPlaces('cafe')}>Cafes</button>
+          </div>
+        </div>
+      </div>
+      <div className="places-list">
+        <h3>{place_type}</h3>
+        <ol>
+          {places.map((place, index) => (
+            <li key={index}>{place.user_name}</li>
           ))}
-          {markerPosition && (
-            <Marker anchor={markerPosition} payload={username} onClick={({ event, anchor, payload }) => {}} />
-          )}
-        </Map>
+        </ol>
       </div>
     </div>
   );
