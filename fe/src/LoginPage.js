4bc4736641181cf98a355badbd6816170b429134
sachshan <sachdeva.sh@northeastern.edu>
2024-06-27 00:56:48 -0400

    Delete user body corrected

--- fe/src/LoginPage.js
+++ fe/src/LoginPage.js
@@ -24,7 +24,7 @@ const LoginPage = () => {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          username,
+          name: username,
           group_key,
         }),
       });
