7376cca463330ab834985cc6ab2d916943e0b8a4
sachshan <sachdeva.sh@northeastern.edu>
2024-06-27 00:33:12 -0400

    Added logger to gunircorn

--- be/flaskr/__init__.py
+++ be/flaskr/__init__.py
@@ -11,8 +11,9 @@ def create_app():
     app = Flask(__name__)
 
     # Configure Flask app logging
-    app.logger.addHandler(logging.StreamHandler(sys.stdout))
-    app.logger.setLevel(logging.INFO)  
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)  
 
     CORS(app, resources={r"/*": {"origins": "*"}})
 
